2302cc0573cafa4663adffe52fc7514ec2a71cb9
Mainul Islam <mainulislam6168@gmail.com>
2024-01-08 16:49:42 +0600

    create, read and delete operation implemented

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express =require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 
 
@@ -27,6 +27,32 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeesCollection = client.db('coffeesDB').collection('coffees');
+
+    // 2. Read all the elements
+    app.get('/coffees', async(req, res) => {
+      const cursor = coffeesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    // 1. Create <---> post
+    app.post('/coffees', async(req, res) =>{
+      const newCoffee = req.body;
+      // console.log(newCoffee)
+      const result = await coffeesCollection.insertOne(newCoffee);
+      res.send(result);
+    })
+
+    // 4. Delete
+    app.delete('/coffees/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await coffeesCollection.deleteOne(query);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
